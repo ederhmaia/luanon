d9b843f41245bffde9ca35272bf815ffc6ca7916
luanon404 <luanon404@gmail.com>
2023-11-18 16:57:58 +0700

    Update js_runtime

--- luanon/js_runtime/js_runtime.js
+++ luanon/js_runtime/js_runtime.js
@@ -2,17 +2,52 @@
 //    Ngày tạo: 11/14/2023
 //    ©2023 LuaNonTeam
 
+module.paths.push(
+    require("child_process").execSync("npm root -g").toString().trim()
+);
+
 const vm = require("vm");
 
 const context = {};
 vm.createContext(context);
 
-process.stdin.on("data", (data) => {
+process.stdin.on("data", (input) => {
     try {
-        const input = JSON.parse(data.toString().trim());
-        process.stdout.write(JSON.stringify({ result: vm.runInContext(atob(input.command), context) || "", error: "" }) + "\n");
+        input = input.toString().trim();
+        let data = JSON.parse(atob(input));
+        let result = vm.runInContext(data.command, context);
+
+        switch (typeof result) {
+            case "boolean":
+            case "number":
+            case "string":
+            case "object":
+                break;
+            case "undefined":
+                result = null;
+                break;
+            default:
+                result = String(result);
+                break;
+        }
+
+        result = "-" + btoa(JSON.stringify({
+            result: result,
+            error: null
+        })) + "-";
+        for (let i = 0; i < result.length; i++) {
+            process.stdout.write(result[i]);
+        }
+        process.stdout.write("\n");
     } catch (error) {
-        process.stdout.write(JSON.stringify({ result: "", error: `NodeError: ${error.name}: ${error.message}` }) + "\n");
+        result = "-" + btoa(JSON.stringify({
+            result: null,
+            error: `NodeError: ${error.stack}`
+        })) + "-";
+        for (let i = 0; i < result.length; i++) {
+            process.stdout.write(result[i]);
+        }
+        process.stdout.write("\n");
     }
 });
 
--- luanon/js_runtime/js_runtime.py
+++ luanon/js_runtime/js_runtime.py
@@ -3,14 +3,25 @@
     Ngày tạo: 11/14/2023
     ©2023 LuaNonTeam
 """
-
+import base64
 import os
 import json
 import queue
-import base64
+import re
 import signal
+import string
 import threading
+import time
+import traceback
 import subprocess
+
+
+class PyError(Exception):
+    pass
+
+
+class NodeError(Exception):
+    pass
 
 
 class JSRuntime:
@@ -18,7 +29,7 @@ class JSRuntime:
     def __init__(self, filename: str = "") -> None:
         # Replace để đảm bảo cái này không bị lỗi prefix \ và /
         filename = os.path.abspath(filename or __file__.replace("js_runtime.py", "js_runtime.js"))
-        self._node: subprocess.Popen = subprocess.Popen(
+        self._node = subprocess.Popen(
             ["node", filename],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
@@ -28,7 +39,7 @@ class JSRuntime:
         )
         self._lock_event = threading.Lock()
         self._stop_event = threading.Event()
-        self._queue: queue.Queue = queue.Queue()
+        self._queue = queue.Queue()
 
     def close(self) -> None:
         if self._node and self._node.poll() is None:
@@ -40,7 +51,7 @@ class JSRuntime:
         self._queue.put(None)
 
     @staticmethod
-    def _create_function_call(function_name: str, *args: str | int | bool) -> str:
+    def _create_function_call(function_name: str, *args: any) -> str:
         if not function_name.strip():
             return ""
 
@@ -57,30 +68,74 @@ class JSRuntime:
 
         return f"{function_name}({', '.join(js_args)})"
 
-    def eval(self, command: str, timeout: int = 30) -> tuple[str | int | bool, str]:
+    def eval(self, command: str, timeout: int = 30) -> tuple[any, any]:
         with self._lock_event:
-            self._node.stdin.write(json.dumps({"command": base64.b64encode(command.encode()).decode(), "timeout": timeout}) + "\n")
+            # Encode command để khỏi xử lý \n hay ' hay " gì đó
+            data = json.dumps({"command": command})
+            data = base64.b64encode(data.encode()).decode()
+            self._node.stdin.write(f"{data}\n")
             self._node.stdin.flush()
+
+            while not self._queue.empty():
+                try:
+                    self._queue.get_nowait()
+                except queue.Empty:
+                    continue
 
             def get_result() -> None:
                 while not self._stop_event.is_set():
                     try:
-                        output = self._node.stdout.readline().strip()
-                        if output:
-                            self._queue.put(tuple(json.loads(output).values()))
-                        else:
-                            self._queue.put(("", "PyError: Kết quả trả về là rỗng."))
-                    except json.decoder.JSONDecodeError as error:
-                        self._queue.put(("", f"PyError: {repr(error)}"))
 
-            threading.Thread(target=get_result, daemon=True).start()
+                        _result = ""
+                        _read_flag = False
+                        _reading_flag = False
+                        while True:
+                            r = self._node.stdout.read(1).strip()
+                            if not _read_flag:
+                                if r == "-":
+                                    _read_flag = True
+                            else:
+                                if r in [
+                                    *list(string.ascii_lowercase),
+                                    *list(string.ascii_uppercase),
+                                    *list(string.digits),
+                                    "+",
+                                    "/",
+                                    "="
+                                ]:
+                                    _result += r
+                                    _reading_flag = True
+                                else:
+                                    if _reading_flag and r == "-":
+                                        break
+
+                        output = base64.b64decode(_result.encode()).decode()
+                        output = json.loads(output)
+                        if output:
+                            self._queue.put(tuple(output.values()))
+                        else:
+                            self._queue.put(("", "PyError: Kết quả trả về là rỗng"))
+                    except json.decoder.JSONDecodeError:
+                        self._queue.put(("", f"PyError: {traceback.format_exc()}"))
+                    return
+
+            dont_call_this_again = threading.Thread(target=get_result, daemon=True)
+            dont_call_this_again.start()
 
             try:
                 result = self._queue.get(timeout=timeout)
             except queue.Empty:
-                result = "", "Hết thời gian chạy lệnh"
-            self._stop_event.set()
+                result = "", "PyError: Hết thời gian chạy lệnh"
+            if result[1]:
+                error = result[1]
+                if "PyError" in error:
+                    raise PyError(error.split(":", 1)[-1].strip())
+                if "NodeError" in error:
+                    raise NodeError(error.split(":", 1)[-1].strip())
+            # Cái dòng chết tiệt này tốn 3 ngày để fix
+            # Vì lý do chết tiệt gì đó mà hàm set() lại không hoạt động
+            self._stop_event.clear()
             return result
 
-    def call(self, function_name: str, *args: str | int | bool, timeout: int = 30) -> tuple[str | int | bool, str]:
+    def call(self, function_name: str, *args: any, timeout: int = 30) -> tuple[any, any]:
         return self.eval(self._create_function_call(function_name, *args), timeout)
